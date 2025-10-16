4ac6ed84615cbf10bf95d6d3c411fc5dbbe2b3ab
ilanaveh <ilana.naveh@mail.huji.ac.il>
2025-10-16 11:24:22 +0300

    Save figure

--- imagenet/weight_analysis/weight_vis.py
+++ imagenet/weight_analysis/weight_vis.py
@@ -9,13 +9,24 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import os
 
+save_fig = True
+
 model_blur = '0'
+ker_size = 22  # size of conv1 kernel; None for original (7)
 # ------------------------------
 # 1. Load trained model
 # ------------------------------
 checkpoint_dir = '/home/projects/bagon/ilanaveh/code/examples_imagenet/imagenet/out/New'
-cp_pth = os.path.join(checkpoint_dir, f'train_resnet_blur{model_blur}', 'model_best.pth.tar')
+model_name = f'train_resnet_blur{model_blur}_ker{ker_size}' if ker_size else f'train_resnet_blur{model_blur}'
+cp_pth = os.path.join(checkpoint_dir, model_name, 'model_best.pth.tar')
 model = models.resnet101()  # initialize base architecture
+
+if ker_size is not None:
+    ori_conv1_ker_size = model.conv1.weight.shape[-1]
+    print("=> Changing conv1 kernel size from: {}, to: {}".format(ori_conv1_ker_size, ker_size))
+    model.conv1 = nn.Conv2d(3, 64, kernel_size=(ker_size, ker_size),
+                            stride=(2, 2), padding=int((ker_size - 1) / 2), bias=False)
+
 model = nn.DataParallel(model)
 checkpoint = torch.load(cp_pth, map_location="cpu")
 
@@ -39,17 +50,19 @@ num_filters = weights.shape[0]
 num_cols = 8
 num_rows = num_filters // num_cols
 
-plt.figure(figsize=(num_cols, num_rows))
+f = plt.figure(figsize=(num_cols, num_rows))
 
 for i in range(num_filters):
     # Normalize filter for visualization
     w = weights[i]
     w_min, w_max = w.min(), w.max()
-    # w = (w - w_min) / (w_max - w_min)  # scale to [0,1]
-
+    w = (w - w_min) / (w_max - w_min)  # scale to [0,1]
     plt.subplot(num_rows, num_cols, i+1)
     plt.imshow(w.permute(1, 2, 0).cpu())  # [C,H,W] → [H,W,C]
     plt.axis("off")
 
-plt.suptitle("ResNet-101 First Layer Filters", fontsize=16)
+plt.suptitle(f"{model_name} First Layer Filters", fontsize=16)
+
+if save_fig:
+    f.savefig(os.path.join('from_weight_vis', model_name))
 plt.show()
